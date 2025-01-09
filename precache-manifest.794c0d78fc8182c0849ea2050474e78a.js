31636bdac46e39abb53fa60c05c56b825729bf28
Dmitriy <145110913+heccberte@users.noreply.github.com>
2025-01-09 09:51:34 +0300

    Updates

---
+++ precache-manifest.794c0d78fc8182c0849ea2050474e78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00141f4a36237394db4928c5dcaf718",
+    "url": "/Marvel-Hero/index.html"
+  },
+  {
+    "revision": "2c19172a6f155311d7ad",
+    "url": "/Marvel-Hero/static/css/main.dfcc0510.chunk.css"
+  },
+  {
+    "revision": "b4bf39cfbf6ecd607be1",
+    "url": "/Marvel-Hero/static/js/2.2faf31e3.chunk.js"
+  },
+  {
+    "revision": "2c19172a6f155311d7ad",
+    "url": "/Marvel-Hero/static/js/main.b68c3fd6.chunk.js"
+  },
+  {
+    "revision": "1deff52b3e567a993387",
+    "url": "/Marvel-Hero/static/js/runtime~main.99f2160a.js"
+  },
+  {
+    "revision": "dd8446a3584cb7275fb1f2f31ab4c950",
+    "url": "/Marvel-Hero/static/media/mjolnir.dd8446a3.png"
+  },
+  {
+    "revision": "fa647f806eff4631b57c14ab8ea8a4aa",
+    "url": "/Marvel-Hero/static/media/thor.fa647f80.jpeg"
+  }
+]);
